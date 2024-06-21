30466917fdde04ef70ad19413e099cf0264ae6be
reddymanyam <reddappareddy18@gmail.com>
2024-06-21 21:14:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Poke from './components/Poke';
+
+function App() {
+  return (
+    <div className="App">
+      <Poke />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Poke.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "./PokeApp.css";
+
+const PokeApp = () => {
+    const [pokemons, setPokemons] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const fetchPokemons = async () => {
+        try {
+            const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100');
+            const results = response.data.results;
+            const detailedPokemons = await Promise.all(results.map(async (pokemon) => {
+                const pokemonData = await axios.get(pokemon.url);
+                return pokemonData.data;
+            }));
+            setPokemons(detailedPokemons);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchPokemons();
+    }, []);
+
+    const filteredPokemons = pokemons.filter(pokemon =>
+        pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return (
+        <div className="poke-app">
+            <h1>Pokémon List</h1>
+            <input
+                type="text"
+                placeholder="Search Pokémon"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <div className="poke-container">
+                {filteredPokemons.map(pokemon => (
+                    <div className="poke-card" key={pokemon.id}>
+                        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                        <h3>{pokemon.name}</h3>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default PokeApp;
